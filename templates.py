ba9baef1300077285a81a2c92837ffa8b1fd33c6
Alex Ramsdell <foundling@users.noreply.github.com>
2015-04-04 15:19:30 -0400

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 '''
-  a simple demo of templating in Python
+  a demo of templating in Python
 '''
 
 from string import Template
 
-# The '$' is peeled off and the remaining part is used as the key
-# we use the key to get the value in the 'values' dictionary
-# the value that the key gets us is then inserted into the string
+# The '$' in the template string is peeled off and the remaining part is used as the key.
+#
+# We use the key to get the value in the 'values' dictionary, which holds the sort of 
+# values that we want to push into a template.
+#
+# The value that the key gets us is then inserted into the string.
 
-template_string = 'Dear $designator $first_name $last_name,\nThis is spam. Sorry.\nRegards,\nSpam, Inc.'
+template_string = 'Dear $designator $first_name $last_name,
+This is spam. Sorry.
+Regards,
+Spam, Inc.'
 values = dict(designator='Mr.',first_name='Michael',last_name='Jackson')
 
 template = Template(template_string)
