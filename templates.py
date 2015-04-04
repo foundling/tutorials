6aa9e814317b24c949b83ec604008ba008a0b4c1
Alex Ramsdell <foundling@users.noreply.github.com>
2015-04-04 15:13:54 -0400

    Update templates.py

--- templates.py
+++ templates.py
@@ -5,8 +5,13 @@
 
 from string import Template
 
-template_string = '$1 $2 $3'
+# The '$' is peeled off and the remaining part is used as the key
+# we use the key to get the value in the 'values' dictionary
+# the value that the key gets us is then inserted into the string
+
+template_string = 'Dear $designator $first_name $last_name,\nThis is spam. Sorry.\nRegards,\nSpam, Inc.'
+values = dict(designator='Mr.',first_name='Michael',last_name='Jackson')
+
 template = Template(template_string)
-values = dict(a=1,b=2,c=3)
-t.safe_substitute(values)
-t.substitute(values)
+template.safe_substitute(values)
+template.substitute(values)
