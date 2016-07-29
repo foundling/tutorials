7dcc316dc0461b9d5e41a3b34384b0c73ad4e1f2
Alex Ramsdell <alexramsdell@gmail.com>
2016-07-29 14:35:21 -0700

    sort works as a component

--- javascript/canjs/sortable_table/app/components/data_table/data_table.js
+++ javascript/canjs/sortable_table/app/components/data_table/data_table.js
@@ -15,9 +15,29 @@ steal(
 
         var sortKey = null;
         return can.Component.extend({
-
             tag: 'data-table',
             template: dataTableView,
+            viewModel: {
+                sortKey: '',
+                sortRows: function(context, element, event) {
+
+                    var comparator = element.attr('id').split('-')[1];
+                    var isSorted = element.hasClass('sorted');
+
+                    //console.log(this.attr('sortKey'), comparator);
+
+                    if (comparator === this.attr('sortKey')) {
+                        this.attr('sortKey', null);
+                        this.attr('employees').reverse();
+                        element.removeClass('sorted');
+                    } else {
+                        element.addClass('sorted');
+                        this.attr('sortKey', comparator);
+                        this.employees.sort(this.attr('sortKey'));
+                    }
+
+                },
+            },
         });
     }
 );
