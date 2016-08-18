6b2973d5a2a98d463c1f8747710a3978042ff0fc
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-17 23:23:18 -0400

    add a little bit to callbacks.js

---
+++ javascript/async/callbacks.js
@@ -0,0 +1,24 @@
+/*
+ * Callbacks are functions passed into a function by name. Anywhere in the function, that callaback can be called.  
+ *
+ *
+ */
+
+var transform = (list, callback) => {
+
+    var new_list = [];
+    var i,
+        max;
+
+    for (i = 0, max = list.length; i < max; ++i) {
+        new_list.push( callback( list[i]) );
+    } 
+
+    return new_list;
+} 
+
+var list = [1,2,3,4,5];
+var doubledList = transform(list, function(element) {
+    return element * 2;
+});
+console.log(doubledList);
